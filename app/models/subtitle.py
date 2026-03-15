550148927e31d8232013b360ee37289f5669be16
Dunsensei <63134711+duanhap@users.noreply.github.com>
2026-03-15 23:03:01 +0700

    cập nhật bảng video và subtitle

--- app/models/subtitle.py
+++ app/models/subtitle.py
@@ -1,7 +1,12 @@
 class Subtitle:
-    def __init__(self, id=None, source_url=None, video_id=None):
+    def __init__(self, id=None, start_time=None, end_time=None, content=None, 
+                 pronunciation=None, translation=None, video_id=None):
         self.id = id
-        self.source_url = source_url
+        self.start_time = start_time
+        self.end_time = end_time
+        self.content = content
+        self.pronunciation = pronunciation
+        self.translation = translation
         self.video_id = video_id
 
     @staticmethod
@@ -9,11 +14,21 @@ class Subtitle:
         if not data: return None
         return Subtitle(
             id=data.get('Id'),
-            source_url=data.get('SourceUrl'),
+            start_time=data.get('StartTime'),
+            end_time=data.get('EndTime'),
+            content=data.get('Content'),
+            pronunciation=data.get('Pronunciation'),
+            translation=data.get('Translation'),
             video_id=data.get('VideoId')
         )
 
     def to_dict(self):
         return {
-            'id': self.id, 'source_url': self.source_url, 'video_id': self.video_id
+            'id': self.id, 
+            'start_time': self.start_time, 
+            'end_time': self.end_time,
+            'content': self.content,
+            'pronunciation': self.pronunciation,
+            'translation': self.translation,
+            'video_id': self.video_id
         }
