89522eeabd421cf4e9b1e5371edd5b690518f372
Dunsensei <63134711+duanhap@users.noreply.github.com>
2026-03-24 17:35:33 +0700

    câp nhat bảng streakdate

--- app/models/streak_date.py
+++ app/models/streak_date.py
@@ -1,12 +1,13 @@
 class StreakDate:
     def __init__(self, id=None, date=None, protected_date=False, protected_by=None, 
-                 xp_earned=0, streak_id=None):
+                 xp_earned=0, streak_id=None, user_id=None):
         self.id = id
         self.date = date
         self.protected_date = protected_date
         self.protected_by = protected_by
         self.xp_earned = xp_earned
         self.streak_id = streak_id
+        self.user_id = user_id
 
     @staticmethod
     def from_dict(data):
@@ -17,7 +18,8 @@ class StreakDate:
             protected_date=bool(data.get('ProtectedDate')),
             protected_by=data.get('ProtectedBy'),
             xp_earned=data.get('ExperiencePointsEarned', 0),
-            streak_id=data.get('StreakId')
+            streak_id=data.get('StreakId'),
+            user_id=data.get('UserId')
         )
 
     def to_dict(self):
@@ -27,5 +29,6 @@ class StreakDate:
             'protected_date': self.protected_date,
             'protected_by': self.protected_by,
             'xp_earned': self.xp_earned,
-            'streak_id': self.streak_id
+            'streak_id': self.streak_id,
+            'user_id': self.user_id
         }
